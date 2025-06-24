677493aa52cc91fc19036507281fbcc5a63d617e
SChung97 <steph9chung@hotmail.com>
2025-06-24 12:32:14 +0100

    made home a clickable link within header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 function Header({ list }) {
   return (
     <>
       <section className="header">
         <h1>NC NEWS</h1>
-        <h2>Home</h2>
+        <h2>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+        </h2>
         <h2>Topics</h2>
       </section>
     </>
