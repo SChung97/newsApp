6da4980f73edc12613c474d2ff9d5af3f79565c0
SChung97 <steph9chung@hotmail.com>
2025-07-03 18:21:37 +0100

    Addition of app bar and styling headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
 import "../App.css";
-function Header({ list }) {
+import { AppBar } from "@mui/material";
+
+function Header() {
   return (
     <>
-      <header className="header">
-        <h1>
-          <Link to="/" className="title-link-to-home">
-            NC NEWS
-          </Link>
-        </h1>
-        <h2><Link to='/topics' className='link-to-topics'>Topics</Link></h2>
-      </header>
+      <AppBar position="static" color="">
+        <header className="header">
+          <h1>
+            <Link to="/" className="title-link-to-home">
+              NC NEWS
+            </Link>
+          </h1>
+          <h2>
+            <Link to="/topics" className="link-to-topics">
+              Topics
+            </Link>
+          </h2>
+          <h2>
+            <Link to="/users" className="link-to-users">
+              Users
+            </Link>
+          </h2>
+        </header>
+      </AppBar>
     </>
   );
 }
--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,69 +1,78 @@
-import { getAllTopics } from "../api"
-import { useEffect, useState, useContext } from "react"
-import { Link } from "react-router-dom"
-import '../App.css'
-import { TopicContext } from "./context/TopicContext"
+import { getAllTopics } from "../api";
+import { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import { TopicContext } from "./context/TopicContext";
 
 function Topics() {
-    const {selectedTopic, handleTopic} = useContext(TopicContext)
-   
+  const { selectedTopic, handleTopic } = useContext(TopicContext);
 
- const [allTopics, setAllTopics] = useState([])
-const [isLoading, setIsLoading] = useState(true)
-const [isError, setIsError] = useState(null)
-console.log('rendering all Topics')
+  const [allTopics, setAllTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
+  console.log("rendering all Topics");
 
-useEffect(() => {
-    console.log('all topics useEffect fired')
-    setIsLoading(true)
-    setIsError(false)
+  useEffect(() => {
+    console.log("all topics useEffect fired");
+    setIsLoading(true);
+    setIsError(false);
 
     getAllTopics()
-    .then((topicsList) => {
-        setAllTopics(topicsList)
-        setIsLoading(false)
-    }).catch((error) => {
-        console.error('Error fetching topics', error)
-        setIsError(true)
-        setIsLoading(false)
-    })
-}, [])
+      .then((topicsList) => {
+        setAllTopics(topicsList);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching topics", error);
+        setIsError(true);
+        setIsLoading(false);
+      });
+  }, []);
 
-const handleTopicSelect = (slug) => {
-    handleTopic(slug)
-}
+  const handleTopicSelect = (slug) => {
+    handleTopic(slug);
+  };
 
-if (isLoading) {
-    return <p>Loading topics</p>
-}
-if (isError) {
-    return <p>Error loading topics</p>
-}
-if (!allTopics || allTopics.length === 0) {
-    return <p>No topics to display</p>
-}
+  if (isLoading) {
+    return <p>Loading topics</p>;
+  }
+  if (isError) {
+    return <p>Error loading topics</p>;
+  }
+  if (!allTopics || allTopics.length === 0) {
+    return <p>No topics to display</p>;
+  }
 
-return (
+  return (
     <>
-    <nav className='topics-nav' > 
-        <Link to='/' onClick={() => handleTopicClick(null)} className={!selectedTopic ? 'active-link' : 'topic-link'}>
-            <h3>All Articles</h3>
+      <nav className="topics-nav">
+        <Link
+          to="/"
+          onClick={() => handleTopicClick(null)}
+          className={!selectedTopic ? "active-link" : "topic-link"}
+        >
+          <h3>All Articles</h3>
         </Link>
         {allTopics.map((topic) => {
-            return (<div key={topic.slug}>
-                <Link to={`/topics/${topic.slug}`} onClick={() => handleTopicSelect(topic.slug)} className={selectedTopic === topic.slug ? 'active-link' : 'topic-link'}>
-                <h3>{topic.slug}</h3>
-                </Link>
-                <p>{topic.description}</p>
-                <img className="topic-img" src={topic.img_url} alt={topic.slug}/>
-                </div>
-            )
+          return (
+            <div key={topic.slug}>
+              <Link
+                to={`/topics/${topic.slug}`}
+                onClick={() => handleTopicSelect(topic.slug)}
+                className={
+                  selectedTopic === topic.slug ? "active-link" : "topic-link"
+                }
+              >
+                <h4>{topic.slug}</h4>
+              </Link>
+              <p>{topic.description}</p>
+              <img className="topic-img" src={topic.img_url} alt={topic.slug} />
+            </div>
+          );
         })}
-        
-    </nav>
+      </nav>
     </>
-)
+  );
 }
 
-
-export default Topics
+export default Topics;
